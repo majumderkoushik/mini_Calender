25ea655893a081765f8ba6fe8ac153ec244d0bab
majumderkoushik <majumderkoushikme@gmail.com>
2023-09-01 20:46:34 +0530

    pushed

---
+++ script.js
@@ -0,0 +1,18 @@
+const monthNameEl = document.getElementById("monthName");
+const dayNameEl = document.getElementById("dayName");
+const dayNumEl = document.getElementById("dayNumber");
+const yearEl = document.getElementById("year");
+
+const date = new Date();
+const month = date.getMonth();
+monthNameEl.innerText = date.toLocaleString("en", {
+  month: "long",
+});
+
+dayNameEl.innerText = date.toLocaleString("en", {
+  weekday: "long",
+});
+
+dayNumEl.innerText = date.getDate();
+
+yearEl.innerText = date.getFullYear();
